8458a53567cd211211ea7fb4b0bc8d9ac88eedf7
zhenya yang <zhenya.yang@breville.com>
2023-02-11 20:36:09 +1100

    move loop to core 1

--- main/main.c
+++ main/main.c
@@ -12,17 +12,21 @@
 // Something must be wrong with this serial write, but I don't have time to investigate into it
 // I simply offset it
 #define OFFSET 50
-void send(const uint8_t *buf, uint32_t len) {
-    for (int i=0; i<len; i++) {
+void send(const uint8_t* buf, uint32_t len)
+{
+    for (int i = 0; i < len; i++)
+    {
         printf("%02x", buf[i]);
     }
 }
 
-void send_int(uint32_t i) {
+void send_int(uint32_t i)
+{
     send((uint8_t*)&i, 4);
 }
 
-void send_packet(uint32_t ts_sec, uint32_t ts_usec, uint32_t len, const uint8_t* buf) {
+void send_packet(uint32_t ts_sec, uint32_t ts_usec, uint32_t len, const uint8_t* buf)
+{
     uint32_t incl_len = len > 2000 ? 2000 : len;
 
     printf("Packet % 3d.%06d %d %d\n", ts_sec, ts_usec, incl_len, len);
@@ -36,25 +40,17 @@ void send_packet(uint32_t ts_sec, uint32_t ts_usec, uint32_t len, const uint8_t*
     printf("\n");
 }
 
-void sniffer_callback(void *buf, wifi_promiscuous_pkt_type_t type){
+void sniffer_callback(void* buf, wifi_promiscuous_pkt_type_t type)
+{
     const wifi_promiscuous_pkt_t* pkt = (wifi_promiscuous_pkt_t*)buf;
     const wifi_pkt_rx_ctrl_t ctrl = (wifi_pkt_rx_ctrl_t)pkt->rx_ctrl;
 
     int64_t t = esp_timer_get_time();
-    send_packet(t/1000000, t%1000000, ctrl.sig_len-4, pkt->payload);
+    send_packet(t / 1000000, t % 1000000, ctrl.sig_len - 4, pkt->payload);
 }
 
-void app_main() {
-    nvs_flash_init();
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-    ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
-    ESP_ERROR_CHECK(esp_wifi_start());
-    esp_wifi_set_promiscuous(true);
-    // Our connectivity bootloader has a wdt set up, so we need to reset it, if we want to use this firmware together with our bootloader
-    // Nothing bad too
-    rtc_wdt_feed();
+static void core1Loop()
+{
     while (true)
     {
         int c = fgetc(stdin);
@@ -67,4 +63,19 @@ void app_main() {
         }
         vTaskDelay(pdMS_TO_TICKS(100));
     }
+}
+
+void app_main()
+{
+    nvs_flash_init();
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+    ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
+    ESP_ERROR_CHECK(esp_wifi_start());
+    esp_wifi_set_promiscuous(true);
+    // Our connectivity bootloader has a wdt set up, so we need to reset it, if we want to use this firmware together with our bootloader
+    // Nothing bad too
+    rtc_wdt_feed();
+    xTaskCreatePinnedToCore(core1Loop, "Core1Loop", 4096, (void*)1, 1, NULL, 1);
 }
